9ea3a9f7052b0ef63a2f6a358b783d2ca55602e3
Max Vasyuk <max234858100@gmail.com>
2019-11-30 00:56:41 +0500

    Form has been added

--- MemoryTrainer/Memory Trainer/FormMemoryTrainer.cs
+++ MemoryTrainer/Memory Trainer/FormMemoryTrainer.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Memory_Trainer
@@ -24,7 +17,12 @@ namespace Memory_Trainer
 
         private void buttonLostWord_Click(object sender, EventArgs e)
         {
-            return;
+            FormLostWord formLW = new FormLostWord();
+            formLW.Left = Left;
+            formLW.Top = Top;
+            Hide();
+            formLW.ShowDialog();
+            Show();
         }
 
         private void buttonQuadShulte_Click(object sender, EventArgs e)
