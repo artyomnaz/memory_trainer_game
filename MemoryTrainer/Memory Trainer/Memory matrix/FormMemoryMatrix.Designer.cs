ac47a303ebe71f36bc2344f440cdd711c15d2a0e
yanatibelius <yana@tibelius.ru>
2019-12-12 00:18:31 +0500

    GameProcess has been added

--- MemoryTrainer/Memory Trainer/Memory matrix/FormMemoryMatrix.Designer.cs
+++ MemoryTrainer/Memory Trainer/Memory matrix/FormMemoryMatrix.Designer.cs
@@ -35,8 +35,7 @@
             // 
             // timer
             // 
-            this.timer.Enabled = true;
-            this.timer.Interval = 1000;
+            this.timer.Interval = 700;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // FormMemoryMatrix
--- MemoryTrainer/Memory Trainer/Memory matrix/FormMemoryMatrix.cs
+++ MemoryTrainer/Memory Trainer/Memory matrix/FormMemoryMatrix.cs
@@ -9,24 +9,72 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//using System.Drawing.Text;
+//using System.IO;
+//using System.Runtime.InteropServices;
+//using System.Text.RegularExpressions;
+
 namespace Memory_Trainer.Memory_matrix
 {
-    public partial class FormMemoryMatrix : Form
+    public partial class FormMemoryMatrix : Form, IGameInterface
     {
         private DataGridView FigureGrid;
+        private Label label;
         private int n;
         private int m;
+        private int Level;
         private int[][] pos;
         private int timerValue;
+
+        enum pos_state {NoColor, WithColor, ForAnimation, Chosen};
+        //public PrivateFontCollection private_fonts = new PrivateFontCollection();
 
         public FormMemoryMatrix()
         {
             InitializeComponent();
-            FigureGrid = new DataGridView();
-            FigureGrid.Parent = this;
-            n = 5;
+        }
+
+        //private void LoadFont()
+        //{
+            //using (MemoryStream fontStream = new MemoryStream(Resource1.Montserrat_ExtraBold))
+           // {
+               // IntPtr data = Marshal.AllocCoTaskMem((int)fontStream.Length);
+               // byte[] fontdata = new byte[fontStream.Length];
+               // fontStream.Read(fontdata, 0, (int)fontStream.Length);
+               // Marshal.Copy(fontdata, 0, data, (int)fontStream.Length);
+               // private_fonts.AddMemoryFont(data, (int)fontStream.Length);
+               // fontStream.Close();
+               // Marshal.FreeCoTaskMem(data);
+            //}
+        //}
+
+        private void FormMemoryMatrix_Load(object sender, EventArgs e)
+        {
+            Level = 1;
+            label = new Label();
+            label.Parent = this;
+            label.BackColor = Color.Transparent;
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            //label.Font = new Font(private_fonts.Families[0], 25F);
+            label.UseCompatibleTextRendering = true;
+            //label.TextRenderingHint = TextRenderingHint.AntiAlias;
+            label.Width = 200;
+            label.Height = 200;
+            label.ForeColor = Color.Black;
+            label.Text = "Уровень: " + Level.ToString();
+            timerValue = 0;
+            n = 3;
             m = (int)(n * n * 0.25);
             pos = new int[n][];
+            CreateFormGrid();
+            RandomPosition();
+            Start();
+        }
+
+        private void CreateFormGrid()
+        {
+            FigureGrid = new DataGridView();
+            FigureGrid.Parent = this;
             FigureGrid.ColumnCount = n;
             for (int i = 0; i < n; i++)
             {
@@ -34,17 +82,27 @@ namespace Memory_Trainer.Memory_matrix
                 var row = new DataGridViewRow();
                 for (int j = 0; j < n; j++)
                 {
-                    pos[i][j] = 0;
+                    pos[i][j] = (int)pos_state.NoColor;
                     var cell = new DataGridViewTextBoxCell();
                     row.Cells.Add(cell);
                 }
                 FigureGrid.Rows.Add(row);
-            }  
+            }
+
+            SetSettings();
         }
-        
-        private void FormMemoryMatrix_Load(object sender, EventArgs e)
+
+        private void SetSettings()
         {
-            timerValue = 0;
+            FigureGrid.AllowUserToResizeRows = false;
+            FigureGrid.AllowDrop = false;
+            FigureGrid.AllowUserToAddRows = false;
+            FigureGrid.AllowUserToDeleteRows = false;
+            FigureGrid.AllowUserToOrderColumns = false;
+            FigureGrid.AllowUserToResizeColumns = false;
+            FigureGrid.AllowUserToResizeRows = false;
+            FigureGrid.DefaultCellStyle.SelectionBackColor = Color.Transparent;
+            FigureGrid.Click += null;
 
             FigureGrid.Width = 2 * this.Height / 3;
             FigureGrid.Height = 2 * this.Height / 3;
@@ -58,41 +116,33 @@ namespace Memory_Trainer.Memory_matrix
 
             FigureGrid.ScrollBars = ScrollBars.None;
             FigureGrid.MultiSelect = false;
-            FigureGrid.CellClick += onClick;
 
-            RandomPosition();
-
-            for (int i = 0; i < n; i++){
+            for (int i = 0; i < n; i++)
+            {
                 FigureGrid.Rows[i].Height = FigureGrid.Height / n;
                 FigureGrid.Columns[i].Width = FigureGrid.Width / n;
                 for (int j = 0; j < n; j++)
                 {
+                    pos[i][j] = (int)pos_state.NoColor;
                     FigureGrid.Rows[i].Cells[j].Selected = false;
                     FigureGrid.Rows[i].Cells[j].Style.BackColor = Color.WhiteSmoke;
                 }
             }
-            FigureGrid.ReadOnly = true;
-            FigureGrid.AllowUserToResizeRows = false;
-            FigureGrid.AllowDrop = false;
-            FigureGrid.AllowUserToAddRows = false;
-            FigureGrid.AllowUserToDeleteRows = false;
-            FigureGrid.AllowUserToOrderColumns = false;
-            FigureGrid.AllowUserToResizeColumns = false;
-            FigureGrid.AllowUserToResizeRows = false;
-            FigureGrid.DefaultCellStyle.SelectionBackColor = Color.Transparent;
 
         }
 
         private void onClick(object sender, EventArgs e)
         {
+            if (timer.Enabled)
+                return;
             int i = (sender as DataGridView).CurrentCell.RowIndex;
             int j = (sender as DataGridView).CurrentCell.ColumnIndex;
-            
-            FigureGrid.Rows[i].Cells[j].Style.SelectionBackColor = Color.Transparent;
+
+            GameProcess(i, j);
         }
 
         private void RandomPosition()
-        {
+        { 
             var r = new int[m];
             Random random = new Random();
             for (int i = 0; i < m; i++) {
@@ -109,44 +159,163 @@ namespace Memory_Trainer.Memory_matrix
                 for (int j = 0; j < n; j++)
                     for (int k = 0; k < m; k++) 
                         if (r[k] == i * n + j)
-                            pos[i][j] = 2;
+                            pos[i][j] = (int)pos_state.ForAnimation;
 
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
+            DrawField();
+            timerValue++;
 
-            for (int i = 0; i < n; i++) {
+            if (timerValue == m + 1)
+            {
+                Thread.Sleep(1500);
+                ClearGrid();
+                FigureGrid.CellClick += onClick;
+                timerValue = 0;
+                Stop();
+            }
+
+        }
+
+        private void Stop()
+        {
+            timer.Stop();
+            timer.Enabled = false;
+        }
+
+        private void Start()
+        {
+            FigureGrid.CellClick += null;
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        private void ClearGrid()
+        {
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    FigureGrid.Rows[i].Cells[j].Style.BackColor = Color.WhiteSmoke;
+
+        }
+
+        private void GameProcess(int i, int j)
+        {
+            if (pos[i][j] == (int)pos_state.WithColor)
+            {
+                FigureGrid.Rows[i].Cells[j].Style.BackColor = Color.IndianRed;
+                FigureGrid.ClearSelection();
+                pos[i][j] = (int)pos_state.Chosen;
+                if (IsFinish())
+                {
+                    //Thread.Sleep(800);
+                    MessageBox.Show("Все верно");
+                    Level++;
+                    if (Level % 4 == 0) { 
+                        n++;
+                        FigureGrid.Click += null;
+                        AddCells();
+                    }
+                    label.Text = "Уровень: " + Level.ToString();
+                    ClearGrid();
+                    RandomPosition();
+                    Start();
+                }
+            }
+            else if (pos[i][j] == (int)pos_state.NoColor)
+            {
+                DialogResult result = MessageBox.Show("Начать новую игру?", "Неверно!", MessageBoxButtons.YesNo);
+                if (result == DialogResult.No)
+                    Close();
+                Level = 1;
+                label.Text = "Уровень: " + Level.ToString();
+                n = 3;
+                FigureGrid.ColumnCount = n;
+                FigureGrid.RowCount = n;
+                ClearGrid();
+                SetSettings();
+                RandomPosition();
+                Start();
+
+            }
+
+            FigureGrid.ClearSelection();
+
+        }
+
+        private void AddCells()
+        {
+            FigureGrid.ColumnCount = n;
+
+            pos = null;
+            pos = new int[n][];
+           // pos[n - 1] = new int[n];
+            var row = new DataGridViewRow();
+            for (int j = 0; j < n; j++)
+            {
+                pos[j] = new int[n];
+                //pos[j][n - 1] = (int)pos_state.NoColor;
+                //pos[n - 1][j] = (int)pos_state.NoColor;
+                var cell = new DataGridViewTextBoxCell();
+                row.Cells.Add(cell);
+            }
+            FigureGrid.Rows.Add(row);
+
+            SetSettings();
+
+        }
+
+        public void DrawField()
+        {
+            for (int i = 0; i < n; i++)
+            {
                 var flag = false;
                 for (int j = 0; j < n; j++)
-                    if (pos[i][j] == 2) {
+                    if (pos[i][j] == (int)pos_state.ForAnimation)
+                    {
                         FigureGrid.Rows[i].Cells[j].Style.BackColor = Color.IndianRed;
-                        pos[i][j] = 1;
+                        pos[i][j] = (int)pos_state.WithColor;
                         flag = true;
                         break;
                     }
                 if (flag)
                     break;
             }
-
-            timerValue++;
-            if (timerValue == m + 1)
-            {
-                Thread.Sleep(3000);
-                ClearGrid();
-                timerValue = 0;
-                timer.Enabled = false;
-                timer.Stop();   
-            }
-
         }
 
-        private void ClearGrid()
+        public bool IsFinish()
         {
+            bool IsFinish = true;
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
-                    FigureGrid.Rows[i].Cells[j].Style.BackColor = Color.WhiteSmoke;
+                    if (pos[i][j] == (int)pos_state.WithColor)
+                    {
+                        IsFinish = false;
+                        break;
+                    }
+            return IsFinish;
+        }
 
+        public void SaveGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowRules()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowInfo()
+        {
+            throw new NotImplementedException();
         }
 
     }
